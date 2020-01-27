9655239bd268f3f28dbd0a51a0a631cab4693e60
Seerat Ahmed Khan <seeratahmed12@gmail.com>
2020-01-27 20:10:57 +0500

    buid added

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs')
 const path = require('path');
+
+const privateKey  = fs.readFileSync('selfsigned.key', 'utf8');
+const certificate = fs.readFileSync('selfsigned.crt', 'utf8');
+
+const options = {
+    key: privateKey,
+    cert: certificate
+}
 
 const app = express();
 app.use(express.static('build'))
@@ -8,6 +18,8 @@ app.use((req, res, next) => {
     return res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-app.listen(80, () => {
+const server = https.createServer(options, app);
+
+server.listen(80, () => {
     console.log(`Server is up and running at ${80}`)
 })
